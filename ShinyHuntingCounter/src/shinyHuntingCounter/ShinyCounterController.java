010affa2071281aacece4b544c44cd03049091a8
Matty Slyzys <74801971+MattyS7473@users.noreply.github.com>
2020-12-18 21:09:03 -0500

    Set up ShinyCounter.fxml and added the functionality to change scenes

--- ShinyHuntingCounter/src/shinyHuntingCounter/ShinyCounterController.java
+++ ShinyHuntingCounter/src/shinyHuntingCounter/ShinyCounterController.java
@@ -1,5 +1,32 @@
 package shinyHuntingCounter;
 
+
 public class ShinyCounterController {
 	
+	private String name;
+	private String generation;
+	private String method;
+	private String date;
+	private Boolean hasShinyCharm;
+	
+	// Setters
+	public void setName(String newName) {
+		name = newName;
+	}
+	
+	public void setGeneration(String newGeneration) {
+		generation = newGeneration;
+	}
+	
+	public void setMethod(String newMethod) {
+		method = newMethod;
+	}
+	
+	public void setDate(String newDate) {
+		date = newDate;
+	}
+	
+	public void setHasShinyCharm(Boolean newValue) {
+		hasShinyCharm = newValue;
+	}
 }
